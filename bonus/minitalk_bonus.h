443f64bdf81f0e58686f7dea4095a2345bac8824
Ouadie El Fengour <oel-feng@e1r8p12.1337.ma>
2024-07-21 03:20:45 +0100

    Added ft_printf to the utils and bonus header

--- bonus/minitalk_bonus.h
+++ bonus/minitalk_bonus.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minitalk_bonus.c                                   :+:      :+:    :+:   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oel-feng <oel-feng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 04:13:10 by oel-feng          #+#    #+#             */
-/*   Updated: 2024/07/06 04:14:09 by oel-feng         ###   ########.fr       */
+/*   Updated: 2024/07/21 03:16:17 by oel-feng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,8 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <stdbool.h>
+# include <stdarg.h>
+# include <unistd.h>
 
+int		ft_printf(const char *format, ...);
 #endif
---
+++ bonus/utils/ft_printf_bonus.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oel-feng <oel-feng@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/16 20:35:47 by oel-feng          #+#    #+#             */
+/*   Updated: 2024/07/21 03:19:37 by oel-feng         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minitalk_bonus.h"
+
+static void	ft_putchar(char c, int *size)
+{
+	(*size)++;
+	write(1, &c, 1);
+}
+
+static void	ft_putnbr(int nbr, int *size)
+{
+	long	n;
+
+	n = nbr;
+	if (n < 0)
+	{
+		ft_putchar('-', size);
+		n *= -1;
+	}
+	if (n > 9)
+	{
+		ft_putnbr(n / 10, size);
+		ft_putchar((n % 10) + 48, size);
+	}
+	else
+		ft_putchar(n + 48, size);
+}
+
+static void	ft_putstr(char *str, int *size)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+	{
+		ft_putstr("(null)", size);
+		return ;
+	}
+	while (str[i])
+		ft_putchar(str[i++], size);
+}
+
+static void	ft_recognition(va_list args, const char format, int *size)
+{
+	if (format == 'c')
+		ft_putchar(va_arg(args, int), size);
+	else if (format == 's')
+		ft_putstr(va_arg(args, char *), size);
+	else if (format == 'd' || format == 'i')
+		ft_putnbr(va_arg(args, int), size);
+	else
+		ft_putchar(format, size);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	int		size;
+	int		i;
+	va_list	args;
+
+	if (write(1, "", 0) == -1)
+		return (-1);
+	size = 0;
+	i = 0;
+	va_start(args, format);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '\0')
+				break ;
+			ft_recognition(args, format[i], &size);
+		}
+		else
+			ft_putchar(format[i], &size);
+		i++;
+	}
+	va_end(args);
+	return (size);
+}
