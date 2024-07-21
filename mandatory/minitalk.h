0b2c1a929bc5d23944de95daa7497df39558296e
Ouadie El Fengour <oel-feng@e1r8p12.1337.ma>
2024-07-21 03:17:07 +0100

    Added ft_printf to the mandatory header

--- mandatory/minitalk.h
+++ mandatory/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: oel-feng <oel-feng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 03:29:21 by oel-feng          #+#    #+#             */
-/*   Updated: 2024/07/06 04:14:00 by oel-feng         ###   ########.fr       */
+/*   Updated: 2024/07/21 01:29:01 by oel-feng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,8 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <stdbool.h>
+# include <stdarg.h>
+# include <unistd.h>
 
+int		ft_printf(const char *format, ...);
 #endif
